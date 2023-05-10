7e87f83de17a8ae30805d044de36570d33533db3
Jacky Ngo <jacky.ngo96@gmail.com>
2023-05-09 20:28:06 -0400

    create sepereate component for navbar

--- DoggoDash/src/components/signedInNav.js
+++ DoggoDash/src/components/signedInNav.js
@@ -15,11 +15,18 @@ const signedOutNavigationLeft = [
   { name: "Search", href: "/search", current: false },
 ];
 
+const signedInNavigationLeft = [
+  { name: "Search", href: "/search", current: false },
+  { name: "Messages", href: "/messages", current: false },
+];
+
+// signedIn ?
+
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function Navbar() {
+export default function SignedInNavbar() {
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
@@ -69,8 +76,8 @@ export default function Navbar() {
 
               <div className="flex flex-1 items-center sm:items-stretch justify-start">
                 <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4 " id="signedOutNavigationLeft">
-                    {signedOutNavigationLeft.map((item) => (
+                  <div className="flex space-x-4 " id="signedInNavigationLeft">
+                    {signedInNavigationLeft.map((item) => (
                       <a
                         key={item.name}
                         href={item.href}
@@ -89,11 +96,11 @@ export default function Navbar() {
                 </div>
               </div>
 
-              <div className="flex flex-1 items-center sm:items-stretch justify-end">
+              {/* <div className="flex flex-1 items-center sm:items-stretch justify-end">
                 <div className="hidden sm:ml-6 sm:block">
-                  <div
-                    className="flex space-x-10" id="signedOutNavigationRight"
-                  >
+                  <div className="flex space-x-10
+
+                  ">
                     {signedOutNavigationRight.map((item) => (
                       <a
                         key={item.name}
@@ -111,7 +118,7 @@ export default function Navbar() {
                     ))}
                   </div>
                 </div>
-              </div>
+              </div> */}
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 {/* NOTIFICATION ICON */}
                 {/* <button
@@ -123,6 +130,62 @@ export default function Navbar() {
                 </button> */}
 
                 {/* Profile dropdown */}
+
+                {/* DISPLAY ONCE LOGGED IN  */}
+                <Menu as="div" className="relative ml-3">
+                  <div>
+                    <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                      <span className="sr-only">Open user menu</span>
+                      <img
+                        className="h-8 w-8 rounded-full"
+                        src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+                        alt=""
+                      />
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="/userProfile"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Your Profile
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="/petProfile"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Pet Profile
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Sign out
+                          </a>
+                        )}
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
               </div>
             </div>
           </div>
