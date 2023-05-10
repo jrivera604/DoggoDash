40745245e1868b76360071594de54e56676cad12
josh rivera <joshrivera91@hotmail.com>
2023-05-09 17:22:48 -0700

    fix db schema

--- DoggoDash/prisma/migrations/20230510002123_schema_prisma/migration.sql
+++ DoggoDash/prisma/migrations/20230510002123_schema_prisma/migration.sql
@@ -1,20 +1,20 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Blog` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
-
-*/
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "Blog";
-PRAGMA foreign_keys=on;
-
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "Post";
-PRAGMA foreign_keys=on;
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "firstname" TEXT,
+    "lastname" TEXT,
+    "streetAddress" TEXT,
+    "city" TEXT,
+    "province" TEXT,
+    "postalCode" TEXT,
+    "email" TEXT,
+    "passworrd" TEXT,
+    "user_type" BOOLEAN,
+    "createdAt" DATETIME,
+    "updatedAt" DATETIME,
+    "rate" INTEGER,
+    "sitterAvailability" TEXT
+);
 
 -- CreateTable
 CREATE TABLE "Message" (
@@ -31,7 +31,7 @@ CREATE TABLE "Message" (
 CREATE TABLE "Dog" (
     "name" TEXT,
     "yearOfBirth" DATETIME,
-    "weight" INTEGER NOT NULL,
+    "weight" INTEGER,
     "temperament" TEXT,
     "comments" TEXT,
     "image" TEXT,
@@ -52,35 +52,11 @@ CREATE TABLE "Breed" (
 -- CreateTable
 CREATE TABLE "DogSitter" (
     "duration" DATETIME,
-    "value" INTEGER NOT NULL,
-    "rating" INTEGER NOT NULL,
+    "value" INTEGER,
+    "rating" INTEGER,
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "sitterId" INTEGER NOT NULL,
     "dogId" INTEGER NOT NULL,
     CONSTRAINT "DogSitter_sitterId_fkey" FOREIGN KEY ("sitterId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT "DogSitter_dogId_fkey" FOREIGN KEY ("dogId") REFERENCES "Dog" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
-
--- RedefineTables
-PRAGMA foreign_keys=OFF;
-CREATE TABLE "new_User" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "firstname" TEXT,
-    "lastname" TEXT,
-    "streetAddress" TEXT,
-    "city" TEXT,
-    "province" TEXT,
-    "postalCode" TEXT,
-    "email" TEXT,
-    "passworrd" TEXT,
-    "user_type" TEXT,
-    "createdAt" DATETIME,
-    "updatedAt" DATETIME,
-    "rate" INTEGER,
-    "sitterAvailability" TEXT
-);
-INSERT INTO "new_User" ("email", "id") SELECT "email", "id" FROM "User";
-DROP TABLE "User";
-ALTER TABLE "new_User" RENAME TO "User";
-PRAGMA foreign_key_check;
-PRAGMA foreign_keys=ON;
