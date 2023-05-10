a3c500bd9e3d31ff97ee8c634d9e42973942ced3
Jacky Ngo <jacky.ngo96@gmail.com>
2023-05-09 22:26:52 -0400

    Change file name

--- DoggoDash/pages/search.js
+++ DoggoDash/pages/search.js
@@ -1,10 +1,12 @@
 
 import Navbar from '@/src/components/nav.js' 
+import SignedInNavbar from '@/src/components/signedInNav.js' 
 
 export default function Search() {
 
   return  (
     <div>
+      <SignedInNavbar/>
       <Navbar/>
     </div>
   )
