1556ed63a7adeb453cad6a124e0a37b6be21374a
josh rivera <joshrivera91@hotmail.com>
2023-05-11 22:26:05 -0700

    add hook secret

--- DoggoDash/pages/api/auth/hooks.js
+++ DoggoDash/pages/api/auth/hooks.js
@@ -4,15 +4,22 @@ const prisma = new PrismaClient();
 
 module.exports = async (req, res) => {
   try {
-    const { email } = JSON.parse(req.body);
-    const user = await prisma.user.create({
-      data: { email },
-    });
-    console.log("created user");
+    const { email, secret } = JSON.parse(req.body);
+
+    if (secret === process.env.AUTH0_HOOK_SECRET) {
+      const user = await prisma.user.create({
+        data: { email },
+      });
+
+      console.log("created user");
+    } else {
+      console.log("You forgot to send me your secret!");
+    }
   } catch (err) {
     console.log(err);
   } finally {
     await prisma.$disconnect();
+
     res.send({ received: true });
   }
-};
+}
